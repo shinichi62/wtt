cf1446982433f4e2e2250ff3afe2b9a2c2460c44
shinichi yamaguchi <shinichi62@gmail.com>
2017-10-26 09:23:40 +0900

    initial commit

---
+++ wtt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wtt
+//
+//  Created by shinichi yamaguchi on 2017/10/26.
+//  Copyright © 2017 shinichi yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
